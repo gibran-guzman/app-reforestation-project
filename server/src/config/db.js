3c330d5c2640416240ef2bde60470752e5bedd71
Gibrán Guzmán <gjv.guzman11@gmail.com>
2026-05-22 23:28:25 -0500

    fix: update pooler username format with project ref and reduce pool size

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,9 +3,10 @@ const logger = require('../utils/logger');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 20,
+  max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
+  statement_timeout: 30000,
   ssl: { rejectUnauthorized: false },
 });
 
