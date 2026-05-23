3eb48ce84008a78d7d55f81c87bb7211c5fa4b40
Gibrán Guzmán <gjv.guzman11@gmail.com>
2026-05-22 21:15:47 -0500

    feat: configure PostgreSQL pool with connection limits and error logging

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+const logger = require('../utils/logger');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+});
+
+pool.on('error', (err) => {
+  logger.error({ err }, 'Unexpected database pool error');
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
-
