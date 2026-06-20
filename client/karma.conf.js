24e08f0fdb4a3f84f06e4834dd47574107f56fb6
Gibrán Guzmán <gjv.guzman11@gmail.com>
2026-06-19 19:58:47 -0500

    chore(client): add karma test runner configuration
    Configure karma with Jasmine framework, ChromeHeadless launcher,
    istanbul coverage reporter with 100% thresholds, and Angular plugin.

---
+++ client/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+  config.set({
+    plugins: ['karma-coverage', 'karma-jasmine', 'karma-chrome-launcher'],
+    frameworks: ['jasmine'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'src/**/!(*.spec).js': ['coverage'],
+    },
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'text-summary' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
+        { type: 'html' },
+      ],
+      includeAllSources: true,
+      check: {
+        global: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100,
+        },
+      },
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+  });
+};
